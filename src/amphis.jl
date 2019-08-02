ebce56a60dc95aebcd6f0e7dfa0fd668ef436e3c
Johannes Terblanche <terblanche.j@gmail.com>
2019-08-02 16:08:25 +0200

    Feature/basics (#1)
    * DFG partially implemented

---
+++ src/amphis.jl
@@ -0,0 +1,146 @@
+#all types shoud inheret from AbstractAmphitheatre and provide a visualize! function
+abstract type AbstractAmphitheatre end
+
+#pose landmark draw property #TODO Move to core?
+mutable struct plDrawProp
+	scale::Float64
+	sphereScale::Float64
+	color::RGBA
+end
+
+using IncrementalInference
+
+plDrawProp() = plDrawProp(0.3, 0.1, RGBA())
+
+# ============================================================
+# --------------------BasicFactorGraphPose--------------------
+# ============================================================
+
+struct BasicFactorGraphPose <: AbstractAmphitheatre
+	robotId::String
+	sessionId::String
+	fg::AbstractDFG
+
+	nodes::Dict{Symbol, AbstractPointPose} #poseId, Softtype
+	meanmax::Symbol
+	zoffset::Float64
+	drawPath::Bool
+	#pose drawing propeties
+	poseProp::plDrawProp
+	#landmark drawing propeties
+	landmarkProp::plDrawProp
+end
+
+
+"""
+    $(SIGNATURES)
+Basic visualizer object to draw poses and landmarks.
+"""
+function BasicFactorGraphPose(robotId::String, sessionId::String, fg::AbstractDFG;
+							  meanmax::Symbol=:max,
+						      zoffset::Float64=0.0,
+							  drawPath::Bool=false,
+							  poseProp::plDrawProp = plDrawProp(0.15, 0.05, RGBA(1,1,0,0.5)),
+							  landmarkProp::plDrawProp = plDrawProp(0.2, 0.1, RGBA(0,1,0,0.5)))
+
+    return BasicFactorGraphPose(robotId, sessionId, fg, Dict{Symbol,AbstractPointPose}(),
+							    meanmax,
+							    zoffset,
+							    drawPath,
+							    poseProp,
+							    landmarkProp)
+end
+
+function BasicFactorGraphPose(fg::AbstractDFG;
+							  meanmax::Symbol=:max,
+						      zoffset::Float64=0.0,
+							  drawPath::Bool=false,
+							  poseProp::plDrawProp = plDrawProp(0.15, 0.05, RGBA(1,1,0,0.5)),
+							  landmarkProp::plDrawProp = plDrawProp(0.2, 0.1, RGBA(0,1,0,0.5)))
+
+    return BasicFactorGraphPose(fg.robotId, fg.sessionId, fg, Dict{Symbol,AbstractPointPose}(),
+							    meanmax,
+							    zoffset,
+							    drawPath,
+							    poseProp,
+							    landmarkProp)
+end
+
+
+"""
+    $(SIGNATURES)
+Basic visualizer object visualize! function.
+"""
+function visualize!(vis::Visualizer, basicfg::BasicFactorGraphPose)::Nothing
+	#TODO maybe improve this function to lower memmory allocations
+
+	fg = basicfg.fg
+
+	robotId = basicfg.robotId
+	sessionId = basicfg.sessionId
+
+	# get all variables
+    vars = getVariables(fg)
+
+
+	basicfg.drawPath && (trackPoints = Point3f0[])
+    # update the variable point-estimate cache
+    for v in vars
+		vsym = v.label
+
+        # get vertex and estimate from the factor graph object
+        X = getKDE(v)
+
+        xmx = basicfg.meanmax == :max ? getKDEMax(X) : getKDEMean(X)
+
+        # get the variable type
+        typestr = split(getData(v).softtype |> typeof |> string, ".")[end]
+		typesym = Symbol(typestr)
+
+		nodef = getfield(Amphitheatre, typesym)
+
+		#NOTE make sure storage order and softtypes are always the same
+		nodestruct = nodef(xmx...)
+
+		if in(:POSE, v.tags)
+			groupsym = :poses
+			drawProp = basicfg.poseProp
+		elseif in(:LANDMARK, v.tags)
+			groupsym = :landmarks
+			drawProp = basicfg.landmarkProp
+
+		# TODO remove? assume l.. is landmark and x.. is pose
+		elseif string(vsym)[1] == 'l'
+			groupsym = :landmarks
+			drawProp = basicfg.landmarkProp
+		elseif string(vsym)[1] == 'x'
+			groupsym = :poses
+			drawProp = basicfg.poseProp
+		elseif in(:VARIABLE, v.tags)
+			@warn "Unknown symbol encountered $vsym"
+			groupsym = :variables
+		end
+
+
+		isnewnode = !haskey(basicfg.nodes, vsym)
+		if isnewnode
+			push!(basicfg.nodes, vsym=>nodestruct)
+		else
+			basicfg.nodes[vsym] = nodestruct
+		end
+
+		visNode!(vis[robotId][sessionId][groupsym][vsym], nodestruct, isnewnode,
+				 zoffset = basicfg.zoffset,
+				 scale = drawProp.scale,
+				 sphereScale = drawProp.sphereScale,
+				 color = drawProp.color)
+
+		#FIXME this is bad, but just testing feature TODO figure out how to do it properly
+		basicfg.drawPath && groupsym == :poses && push!(trackPoints, Point3f0(xmx[1],xmx[2],0.0))
+
+    end
+
+	basicfg.drawPath && setobject!(vis[robotId][sessionId][:track], Object(PointCloud(trackPoints), LineBasicMaterial(), "Line"))
+
+    return nothing
+end
